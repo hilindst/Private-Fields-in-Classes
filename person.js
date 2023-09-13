63dd1df0571f72262345cafdb71b7622c753a977
hilindst <114443718+hilindst@users.noreply.github.com>
2023-09-12 20:05:08 -0500

    New Repository for 9-11
    testing NPM

--- person.js
+++ person.js
@@ -1,5 +1,20 @@
 class Person {
   // Write code here
+  #id;
+  #name;
+
+  constructor (id) {
+   const randomVal =Math.floor(Math.random()*id);
+   this.#id = randomVal;
+  }
+
+  get getID(){
+    return this.#id;
+  }
 }
+const franny = new Person(123);
+const zooey = new Person(456);
+console.log(franny.getID);
+console.log(zooey.getID);//console.log displays 0 for each
 
 module.exports = Person;
